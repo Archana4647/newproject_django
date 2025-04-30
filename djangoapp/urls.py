0bd2963646c3ba3c54efd08764de94260ce7ad18
Archana <archananajay9@gmail.com>
2025-04-30 11:00:06 +0530

    first commit

---
+++ djangoapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .import views as v
+
+urlpatterns =[
+    path('',v.home, name='home'),
+    path('index/',v.index, name='index'),
+    path('about/',v.about ,name='about'),
+    path('base/',v.base, name='base'),
+    path('contact/',v.contact,name='contact'),
+    path('student/',v.students, name='student'),
+    path('newstudent/',v.newstudent,name='newstudent'),
+    path('register/',v.register,name='register'),
+    path('signin/',v.signin,name='signin'),
+    path('logout/', v.uslogout, name='logout'),
+    path('list/',v.stlist,name='list'),
+    path('item/<int:pk>/delete/', v.delt,name='delete'),
+    path('item/<int:pk>/edit/', v.editt, name='edit'),
+]
---
+++ media/documents/assessment.py
@@ -0,0 +1,7 @@
+list=[]
+n=0
+while(n<5):
+    num=int(input("enter a number:"))
+    list.append(num)
+    n=n+1
+print('list=',list)
